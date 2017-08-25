ec3b189ae71e9e98d8892433cd6d59e2a4cd488d
cfc603 <wtrevor162@gmail.com>
2017-08-25 17:53:24 -0400

    add url_for method

--- private/initialize.php
+++ private/initialize.php
@@ -1,3 +1,10 @@
 <?php
     define("WWW_ROOT", '');
+
+    function url_for($path){
+        if($path[0] != "/"){
+            $path = "/" . $path;
+        }
+        return WWW_ROOT . $path;
+    }
 ?>
